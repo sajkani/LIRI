0298e4ba1d5ccc7d2cdf0c0b9dd03798756c7d29
Nicholas R Sajka <nicksajka@gmail.com>
2017-07-04 09:58:06 -0400

    adding twitter keys and random.txt

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'WxUK0j0d5iFqRtfvg3yMleFWi',
+  consumer_secret: 'YiBWpVeTew7StiHPdmUmRFlnXeLOir7NJUMxeeWUtpn3slx4w7',
+  access_token_key: '380727415-7LHzvYVae0sQ9yJLOYrixQfL7c5AgW87pfZp8xOI',
+  access_token_secret: 'UrxP6jGsmG87ca5NPmwJ7aQJ5fV0BWNNrkD2hf56OF52S',
+}
