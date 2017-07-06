d9273dea89aa48e264ed4cc0771ebfffcb82caf9
Nicholas R Sajka <nicksajka@gmail.com>
2017-07-05 22:04:44 -0400

    all working except spotify

--- LIRI.js
+++ LIRI.js
@@ -1,52 +1,166 @@
-//Declar initial API and created variables
-var fs = require('fs');
-var Twitter = require('twitter');
-var spotify = require('spotify');
-var request = require('request');
-var keys = require('./keys.js');
+let spotify = require('spotify');
+let twitter = require('twitter');
+let request = require('request');
+let fs = require("fs");
+let input = process.argv[2];
+let requestID = process.argv[3];
+const keys = require('./keys.js');
 var twitterKeys = keys.twitterKeys;
 var cmdArgs = process.argv;
 var liriCommand = cmdArgs[2];
 var liriArg = '';
 
-for (var i = 3; i < cmdArgs.length; i++) {
-	liriArg += cmdArgs[i] + ' ';
+
+
+switch (input) {
+
+	case "my-tweets":
+	getTweets();
+	break;
+
+	case "spotify-this-song":
+	getSongs( requestID );
+	break;
+
+	case "movie-this":
+	getMovies( requestID );
+	break;
+
+	case "do-what-it-says":
+	random();
+	break;
 }
 
-function retrieveTweets() {
-	fs.appendFile('./log.txt', 'User Command: node liri.js my-tweets\n\n', (err) => {
-		if (err) throw err;
+log();
+
+function getTweets() {
+    fs.appendFile('./log.txt', 'User Command: node liri.js my-tweets\n\n', (err) => {
+        if (err) throw err;
+    });
+    var client = new twitter(twitterKeys);
+
+    var params = {screen_name: 'sajkaniDev', count: 20};
+
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (error) {
+            var errorStr = 'ERROR: Retrieving user tweets -- ' + error;
+
+            fs.appendFile('./log.txt', errorStr, (err) => {
+                if (err) throw err;
+                console.log(errorStr);
+            });
+            return;
+        } else {
+
+            var outputStr = '------------------------\n' +
+                            'My Recent Tweets:\n' + 
+                            '------------------------\n\n';
+
+            for (var i = 0; i < tweets.length; i++) {
+                outputStr += 'Created on: ' + tweets[i].created_at + '\n' + 
+                             'Tweet content: ' + tweets[i].text + '\n' +
+                             '------------------------\n';
+            }
+
+            fs.appendFile('./log.txt', 'LIRI Response:\n\n' + outputStr + '\n', (err) => {
+                if (err) throw err;
+                console.log(outputStr);
+            });
+        }
+    });
+}
+
+function getSongs( songName ) {
+
+
+	spotify.get
+
+	spotify.search({ type: 'track', query: songName }, function(err, data) {
+		console.log(data);
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+
+});
+}
+
+function getMovies( movieTitle ) {
+
+	if (!movieTitle) {
+		movieTitle = "Mr Nobody"
+	}
+	
+	request('http://www.omdbapi.com/?apikey=40e9cece&tomatoes=true&t=' + movieTitle, function (error, response, body) {
+
+		let movie = JSON.parse(body);
+       
+        console.log(" ");
+        console.log(" ");
+        console.log("Displaying Movie Results for: " + movie.Title);
+        console.log("==========================================================================");
+        console.log(" ");
+	    console.log("Title: " + movie.Title);
+        console.log(" ");
+		console.log("Released in: " + movie.Year);
+        console.log(" ");
+		console.log("IMDB Ratiing: " + movie.imdbRating);
+        console.log(" ");
+		console.log("Countries Produced: " + movie.Country);
+        console.log(" ");
+		console.log("Language(s): " + movie.Language);
+        console.log(" ");
+		console.log("Plot: " +movie.Plot);
+        console.log(" ");
+		console.log("Actors: " + movie.Actors);
+        console.log(" ");
+        console.log("Rotten Tomatoes Score: " + movie.Ratings[1].Value);
+        console.log(" ");
+        console.log("Rotten Tomatoes URL: " + movie.tomatoURL);
+        console.log(" ");
+        console.log(" ");
+                
+
 	});
-	var client = new Twitter(twitterKeys);
 
-	var params = {screen_name: 'sajkaniDev', count: 20};
+}
 
-	client.get('statuses/user_timeline', params, function(error, tweets, response) {
-		if (error) {
-			var errorStr = 'ERROR: Retrieving user tweets -- ' + error;
+function random() {
 
-			fs.appendFile('./log.txt', errorStr, (err) => {
-				if (err) throw err;
-				console.log(errorStr);
-			});
-			return;
-		} else {
+	fs.readFile("random.txt", "utf8", function(err, data) {
+		data = data.split(",");
 
-			var outputStr = '------------------------\n' +
-							'User Tweets:\n' + 
-							'------------------------\n\n';
+		input = data[0].trim();
+		requestID = data[1].trim();
 
-			for (var i = 0; i < tweets.length; i++) {
-				outputStr += 'Created on: ' + tweets[i].created_at + '\n' + 
-							 'Tweet content: ' + tweets[i].text + '\n' +
-							 '------------------------\n';
-			}
+		switch (input) {
 
-			fs.appendFile('./log.txt', 'LIRI Response:\n\n' + outputStr + '\n', (err) => {
-				if (err) throw err;
-				console.log(outputStr);
-			});
+			case "my-tweets":
+			getTweet();
+			break;
+
+			case "spotify-this-song":
+			getSongs( requestID );
+			break;
+
+			case "movie-this":
+			getMovies( requestID );
+			break;
+
+			case "do-what-it-says":
+			random();
+			break;
 		}
 	});
 }
-retrieveTweets(); 
+
+function log() {
+
+	if (!process.argv[3]) {
+		fs.appendFile("log.txt", process.argv[2] + ", ");
+	} else {
+
+	fs.appendFile("log.txt", process.argv[2] + " " + process.argv[3] + ", ");
+
+	}
+}
